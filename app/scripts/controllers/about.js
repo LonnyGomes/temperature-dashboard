6b3f580fb4a7a725668ebcef110b82d93c1e621c
Lonny Gomes <code@lonnygomes.com>
2016-08-20 02:22:31 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name temperatureDashboardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the temperatureDashboardApp
+ */
+angular.module('temperatureDashboardApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name temperatureDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the temperatureDashboardApp
+ */
+angular.module('temperatureDashboardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
