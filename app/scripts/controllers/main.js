74755b42dca5e745c3046c224502fd75565986c2
Lonny Gomes <code@lonnygomes.com>
2016-08-22 00:37:06 -0400

    Updated main page to display the temperature directives

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,4 @@
-'use strict';
-
+/*global angular */
 /**
  * @ngdoc function
  * @name temperatureDashboardApp.controller:MainCtrl
@@ -8,10 +7,27 @@
  * Controller of the temperatureDashboardApp
  */
 angular.module('temperatureDashboardApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function (temperatureService) {
+    'use strict';
+    var self = this;
+
+    self.temperatures = {};
+
+    self.temperatures.livingRoom = {};
+    self.temperatures.office = {};
+
+    temperatureService.getCurrentTemperature('office')
+      .then(function (val) {
+        self.temperatures.office = val;
+      }, function (err) {
+        console.error('err:', err);
+      });
+
+    temperatureService.getCurrentTemperature('Living_Room_Thermostat')
+      .then(function (val) {
+        self.temperatures.livingRoom = val;
+      }, function (err) {
+        console.error('err:', err);
+      });
+
   });
