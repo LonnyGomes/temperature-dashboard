109cba7498302f79dd45b7bdeba56ae789e7a04c
Lonny Gomes <code@lonnygomes.com>
2016-08-22 23:34:18 -0400

    Added experimentail socket.io support

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,14 +7,27 @@
  * Controller of the temperatureDashboardApp
  */
 angular.module('temperatureDashboardApp')
-  .controller('MainCtrl', function (temperatureService) {
+  .controller('MainCtrl', function (temperatureService, $timeout) {
     'use strict';
     var self = this;
 
+    var socket = io.connect('http://localhost:3000');
     self.temperatures = {};
 
     self.temperatures.livingRoom = {};
     self.temperatures.office = {};
+
+    socket.on('connect', function () {
+      console.log('got connected!');
+    });
+
+    socket.on('temperatureUpdated', function (data) {
+        data.timeStamp = moment();
+        data.timeString = moment().fromNow();
+        $timeout(function () {
+            self.temperatures[data.deviceName] = data;
+        });
+    });
 
     temperatureService.getCurrentTemperature('office')
       .then(function (val) {
@@ -25,7 +38,7 @@ angular.module('temperatureDashboardApp')
 
     temperatureService.getCurrentTemperature('Living_Room_Thermostat')
       .then(function (val) {
-        self.temperatures.livingRoom = val;
+        self.temperatures.Living_Room_Thermostat = val;
       }, function (err) {
         console.error('err:', err);
       });
